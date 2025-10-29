a20d4513a4e427d37a7bf0396ef6773936b0777c
Abdou <2000abdallah2014@gmail.com>
2025-10-29 18:02:22 +0100

    activate arabic locale

--- src/app.vue
+++ src/app.vue
@@ -5,3 +5,9 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup>
+import { ar } from '~/assets/prime-vue-ar.json'
+const primeConfig = usePrimeVue()
+ primeConfig.config.locale = ar
+
+</script>
