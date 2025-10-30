e612c03eb540a0c506f2d8efb90ba530d0a0636b
Abdou <2000abdallah2014@gmail.com>
2025-10-30 11:00:34 +0100

    added Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import Theme from '@primeuix/themes/aura';
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+    modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss',"@pinia/nuxt"],
     srcDir: 'src/',
     css: [
         '/assets/styles.scss',
