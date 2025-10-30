6c1c82eefe48eca2eb1a890e7deed7de71e10370
Abdou <2000abdallah2014@gmail.com>
2025-10-30 10:11:00 +0100

    typed student endpoints

--- src/server/api/student/index.get.ts
+++ src/server/api/student/index.get.ts
@@ -1,9 +1,10 @@
 import db from '~/db/db';
+import type { Student } from '~/data/types';
 
 export default defineEventHandler(() => {
   const stmt = db.prepare('SELECT * FROM student');
   const students = stmt.all();
   console.log('students: ', students);
-  return students;
+  return students as Student[];
 });
 
--- src/server/api/student/index.post.ts
+++ src/server/api/student/index.post.ts
@@ -1,8 +1,10 @@
+import { Student } from '~/data/types';
 import db from '~/db/db';
 
-export default defineEventHandler(async () => {
-  const stmt = db.prepare('INSERT INTO student (first_name, last_name, father_name, phone_number, birth_date, address) VALUES (?, ?, ?, ?, ?, ?)');
-  const info = stmt.run('abdou', 'diop', 'mamadou', '1234567890', 946684800, '123 Main St');
+export default defineEventHandler(async (event) => {
+  const {first_name, last_name, father_name,grandfather_name, sex, phone_number, birth_date, address} = await readBody<Student>(event) ;
+  const stmt = db.prepare('INSERT INTO student (first_name, last_name, father_name,grandfather_name, sex, phone_number, birth_date, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+  const info = stmt.run(first_name, last_name, father_name,grandfather_name, sex, phone_number, birth_date, address);
 
   return { success: true, id: info.lastInsertRowid, info };
 });
