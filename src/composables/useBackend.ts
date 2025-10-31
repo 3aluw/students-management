d17427a5bedc9026744a2ac4685adb0c75eb4ae1
Abdou <2000abdallah2014@gmail.com>
2025-10-31 21:53:01 +0100

    added delete class to useBackend composable

--- src/composables/useBackend.ts
+++ src/composables/useBackend.ts
@@ -4,8 +4,13 @@ export default function () {
   const getClasses = () => {
     return $fetch<Class[]>("/api/classes");
   };
+  const deleteClass = (classId: string) => {
+    return $fetch(`/api/classes/?id=${classId}`, {
+      method: "DELETE",
+    });
+  }
 
   return {
-    getClasses,
+    getClasses, deleteClass
   };
 }
