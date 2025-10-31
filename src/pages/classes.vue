99398af9e873f624e551f33d365fa60edd952025
Abdou <2000abdallah2014@gmail.com>
2025-10-31 21:50:26 +0100

    created classes page

--- src/pages/classes.vue
+++ src/pages/classes.vue
@@ -1,13 +1,65 @@
 <template>
-  <div className="card">
-    <div class="font-semibold text-xl mb-4">Empty Page</div>
-    <p>Use this page to start from scratch and place your custom content.</p>
-    {{ studentStore.classes }}
+  <div class="card">
+    <div class="font-semibold text-xl mb-4">الأقسام
+            <Button class="mx-4" icon="pi pi-plus" severity="contrast" text raised rounded />
+    </div>
+    <DataTable :value="studentStore.classes" scrollable scrollHeight="400px" class="mt-6">
+      <Column field="level" header="المستوى" style="min-width: 200px" class="font-bold"></Column>
+      <Column field="abbreviation" header="الحرف" style="min-width: 200px"> </Column>
+      <Column header="تعديل" style="min-width: 50px">
+        <template #body>
+          <Button icon="pi pi-pencil" severity="secondary" rounded />
+        </template>
+      </Column>
+      <Column header="حذف" style="min-width: 50px">
+        <template #body="slotProps">
+          <Button @click="handleDeleteClick(slotProps.data.id)" icon="pi pi-trash" severity="danger" rounded />
+        </template>
+      </Column>
+    </DataTable>
+    <UtilsConfirmDialog v-model="showConfirmDialog" :danger="true"
+      message="هل أنت متأكد من رغبتك في حذف هذا القسم؟ كل تلاميذ القسم سيتم حذفهم تلقائيا"
+      @confirm="handleConfirmDelete">
+    </UtilsConfirmDialog>
   </div>
+
 </template>
 <script setup lang="ts">
+import { Toast } from 'primevue';
+import type { Class } from '~/data/types'
 import { useStudentStore } from '~/store/studentStore';
+const studentStore = useStudentStore()
+const backend = useBackend()
 
-const studentStore = useStudentStore() 
+const classToEdit = ref<Class | undefined>(undefined)
+const classIdToDelete = ref<string | undefined>(undefined)
+
+const handleDeleteClick = (classId: string) => {
+  classIdToDelete.value = classId
+  showConfirmDialog.value = true
+}
+const showConfirmDialog = ref(false)
+
+const handleConfirmDelete = (confirmed: boolean) => {
+  if (!confirmed || !classIdToDelete.value) {
+    classIdToDelete.value = undefined
+    return
+  }
+  executeDelte(classIdToDelete.value)
+}
+
+const executeDelte = async (classId: string) => {
+  try {
+    await backend.deleteClass(classId)
+    await studentStore.populateClasses()
+    classIdToDelete.value = undefined
+    alert('تم حذف القسم بنجاح')
+  } catch (error) {
+alert('حدث خطأ أثناء حذف القسم')
+  }
+}
+const executeEdit = (classId: string) => {
+
+}
 </script>
 <style scoped></style>
