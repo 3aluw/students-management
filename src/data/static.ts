c9860ee7a0f93fc1b185fab94a6dc148a22ac675
Abdou <2000abdallah2014@gmail.com>
2025-11-01 11:38:38 +0100

    added arabic names of  student properties

--- src/data/static.ts
+++ src/data/static.ts
@@ -1,4 +1,4 @@
-import type { Gender } from "./types";
+import type { Gender, Student } from "./types";
 
 export const genderOptions = [
   { label: "ذكر", value: "M" },
@@ -7,3 +7,16 @@ export const genderOptions = [
   label: string;
   value: Gender;
 }[];
+
+export const ArabicStudentProperties : Record<keyof Student,string> ={
+  id: "المعرف",
+  first_name: "الاسم",
+  last_name: "اللقب",
+  father_name: "اسم الأب",
+  grandfather_name: "اسم الجد",
+  class_id: "الصف",
+  sex: "الجنس",
+  phone_number: "رقم الهاتف",
+  birth_date: "تاريخ الميلاد",
+  address: "العنوان",
+}
