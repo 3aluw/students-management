8ed40b063bf6ea7351f6b5092bc4083b01f8e36f
Abdou <2000abdallah2014@gmail.com>
2025-11-01 12:16:00 +0100

    added class support of required field message

--- src/composables/useFormUtils.ts
+++ src/composables/useFormUtils.ts
@@ -1,14 +1,34 @@
-import { ArabicStudentProperties } from "~/data/static";
+import { ArabicStudentProperties, ArabicClassProperties } from "~/data/static";
+import type { Student, Class } from "~/data/types";
 
 export default function () {
+  const formatRequiredFieldMessage = (
+    arabicText: string,
+    type: "text" | "choice" = "text"
+  ) =>
+    type === "text"
+      ? `يجب إدخال ${arabicText}`
+      : type == "choice"
+      ? `يجب اختيار ${arabicText}`
+      : "هذا الحقل مطلوب";
+
   const getRequiredFieldMessage = (
-    fieldName: keyof typeof ArabicStudentProperties,
+    fieldName: keyof typeof ArabicStudentProperties | keyof Class,
     type: "text" | "choice" = "text"
   ) => {
-    return type === "text" ?`يجب إدخال ${ArabicStudentProperties[fieldName]}` 
-        : type == "choice" ? `يجب اختيار ${ArabicStudentProperties[fieldName]}` : "هذا الحقل مطلوب";
+    return fieldName in ArabicStudentProperties
+      ? formatRequiredFieldMessage(
+          ArabicStudentProperties[fieldName as keyof Student],
+          type
+        )
+      : fieldName in ArabicClassProperties
+      ? formatRequiredFieldMessage(
+          ArabicClassProperties[fieldName as keyof Class],
+          type
+        )
+      : "هذا الحقل مطلوب";
   };
   return {
-    getRequiredFieldMessage
+    getRequiredFieldMessage,
   };
 }
