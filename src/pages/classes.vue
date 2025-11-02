883751eccf097a3d22df47aea426e26e59d9a468
Abdou <2000abdallah2014@gmail.com>
2025-11-02 21:38:54 +0100

    add class creation and editing functionality with improved dialog handling

--- src/pages/classes.vue
+++ src/pages/classes.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="card">
     <div class="font-semibold text-xl mb-4">الأقسام
-            <Button class="mx-4" icon="pi pi-plus" severity="contrast" text raised rounded />
+      <Button class="mx-4" icon="pi pi-plus" severity="contrast" text raised rounded @click="showClassDialog = true" />
     </div>
     <DataTable :value="studentStore.classes" scrollable scrollHeight="400px" class="mt-6">
       <Column field="level" header="المستوى" style="min-width: 200px" class="font-bold"></Column>
-      <Column field="abbreviation" header="الحرف" style="min-width: 200px"> </Column>
+      <Column field="section" header="الحرف" style="min-width: 200px"> </Column>
       <Column header="تعديل" style="min-width: 50px">
-        <template #body>
-          <Button icon="pi pi-pencil" severity="secondary" rounded />
+        <template #body="slotProps">
+          <Button @click="handleEditClick(slotProps.data)" icon="pi pi-pencil" severity="secondary" rounded />
         </template>
       </Column>
       <Column header="حذف" style="min-width: 50px">
@@ -21,21 +21,32 @@
       message="هل أنت متأكد من رغبتك في حذف هذا القسم؟ كل تلاميذ القسم سيتم حذفهم تلقائيا"
       @confirm="handleConfirmDelete">
     </UtilsConfirmDialog>
+    <Dialog header="أدخل معلومات القسم" @hide="classToEdit = undefined" v-model:visible="showClassDialog"
+      :style="{ width: '350px' }" :modal="true">
+      <UtilsNewEntityForm entityType="class" :entityObject="classToEdit" @submit="handleSubmit" />
+    </Dialog>
+
   </div>
 
 </template>
 <script setup lang="ts">
-import { Toast } from 'primevue';
-import type { Class } from '~/data/types'
+
+import type { Class, NewClass } from '~/data/types'
 import { useStudentStore } from '~/store/studentStore';
 const studentStore = useStudentStore()
 const backend = useBackend()
+const toast = useToast();
 
+const showClassDialog = ref(false)
 const classToEdit = ref<Class | undefined>(undefined)
-const classIdToDelete = ref<string | undefined>(undefined)
+const classIdToDelete = ref<number | undefined>(undefined)
 
-const handleDeleteClick = (classId: string) => {
-  classIdToDelete.value = classId
+const handleEditClick = (classObj: Class) => {
+  classToEdit.value = classObj;
+  showClassDialog.value = true;
+}
+const handleDeleteClick = (classObj: Class) => {
+  classIdToDelete.value = classObj.id
   showConfirmDialog.value = true
 }
 const showConfirmDialog = ref(false)
@@ -45,21 +56,47 @@ const handleConfirmDelete = (confirmed: boolean) => {
     classIdToDelete.value = undefined
     return
   }
-  executeDelte(classIdToDelete.value)
+  executeDelete(classIdToDelete.value)
 }
 
-const executeDelte = async (classId: string) => {
+const executeDelete = async (classId: number) => {
   try {
     await backend.deleteClass(classId)
     await studentStore.populateClasses()
     classIdToDelete.value = undefined
-    alert('تم حذف القسم بنجاح')
+    toast.add({ severity: 'success', summary: 'تم حذف القسم بنجاح', life: 3000 })
   } catch (error) {
-alert('حدث خطأ أثناء حذف القسم')
+    toast.add({ severity: 'error', summary: 'حدث خطأ أثناء حذف القسم', life: 3000 })
+
   }
 }
-const executeEdit = (classId: string) => {
+const handleSubmit = (newClass: NewClass) => {
+  classToEdit.value ? EditClass({ ...newClass, id: classToEdit.value.id }) : createNewClass(newClass)
+}
 
+const EditClass = async (classObj: Class) => {
+  try {
+    await backend.updateClass(classObj)
+    await studentStore.populateClasses()
+    showClassDialog.value = false
+    toast.add({ severity: 'success', summary: 'تم تعديل القسم بنجاح', life: 3000 })
+
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'حدث خطأ أثناء تعديل معلومات القسم', life: 3000 })
+
+  }
+}
+const createNewClass = async (newClass: NewClass) => {
+  try {
+    await backend.createClass(newClass)
+    await studentStore.populateClasses()
+    showClassDialog.value = false
+    toast.add({ severity: 'success', summary: 'تم إنشاء القسم بنجاح', life: 3000 })
+
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'حدث خطأ أثناء إنشاء القسم', life: 3000 })
+
+  }
 }
 </script>
 <style scoped></style>
