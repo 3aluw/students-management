6b657d77f8b82df38cfdd67c5fbc7bc8be2c8afb
Abdou <2000abdallah2014@gmail.com>
2025-11-02 21:37:53 +0100

    added create / update class functions to useBackend

--- src/composables/useBackend.ts
+++ src/composables/useBackend.ts
@@ -1,16 +1,26 @@
-import type { Class } from "~/data/types";
+import type { Class, NewClass, EditClass } from "~/data/types";
 
 export default function () {
+  const createClass = (body: NewClass) => {
+    return $fetch("/api/classes", {
+      method: "POST",
+      body,
+    });
+  };
+  const updateClass = (body: EditClass) => {
+    return $fetch("/api/classes", {
+      method: "POST",
+      body,
+    });
+  };
   const getClasses = () => {
     return $fetch<Class[]>("/api/classes");
   };
-  const deleteClass = (classId: string) => {
+  const deleteClass = (classId: number) => {
     return $fetch(`/api/classes/?id=${classId}`, {
       method: "DELETE",
     });
-  }
-
-  return {
-    getClasses, deleteClass
   };
+
+  return { createClass, updateClass, getClasses, deleteClass };
 }
