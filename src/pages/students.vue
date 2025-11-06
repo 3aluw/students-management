ea20f20fe91cc551e650beb69d79f14a957ec61f
Abdou <2000abdallah2014@gmail.com>
2025-11-06 21:44:28 +0100

    added dynamic messages to toast / clean up the code in students page

--- src/pages/students.vue
+++ src/pages/students.vue
@@ -6,12 +6,12 @@
                     <Button label="جديد" icon="pi pi-plus" iconPos="right" severity="secondary" class="mx-2"
                         @click="showStudentDialog = true" />
                     <Button label="حذف" icon="pi pi-trash" iconPos="right" severity="secondary" class="mx-2"
-                        @click="deleteStudents(selectedStudents)"
+                        @click="useDeleteConfirm.requestAction(selectedStudents)"
                         :disabled="!selectedStudents || !selectedStudents.length" />
                     <Button label="تعديل" icon="pi pi-pencil" iconPos="right" severity="secondary" class="mx-2"
-                        @click="confirmDeleteSelected" v-if="selectedStudents.length == 1" />
+                        @click="" v-if="selectedStudents.length == 1" />
                     <Button label="نقل إلى" icon="pi pi-undo" iconPos="right" severity="secondary" class="mx-2"
-                        @click="confirmDeleteSelected" v-else-if="selectedStudents.length > 1" />
+                        @click="" v-else-if="selectedStudents.length > 1" />
 
                 </template>
 
@@ -45,7 +45,7 @@
 
                     </div>
                 </template>
-                
+
                 <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
 
                 <Column field="first_name" header="الاسم" class="hidden" />
@@ -72,18 +72,22 @@
             :style="{ width: '350px' }" :modal="true">
             <UtilsNewEntityForm entityType="student" :entityObject="studentToEdit" @submit="handleStudentSubmit" />
         </Dialog>
+        <UtilsConfirmDialog header="حذف الطلبة" :danger="true" v-model="useDeleteConfirm.showConfirm.value"
+            @confirm="useDeleteConfirm.confirmAction" />
     </div>
 </template>
 <script setup lang="ts">
 import { FilterMatchMode } from '@primevue/core/api';
 import { useToast } from 'primevue/usetoast';
 import type { Student, DataTableSlot, NewStudent } from '~/data/types'
+import { userFeedbackMessages } from '~/data/static';
 import { ProductService } from "~/service/ProductService.js";
 import { useStudentStore } from '~/store/studentStore';
 const studentStore = useStudentStore();
 const backend = useBackend()
 const toast = useToast();
 const dt = ref();
+const { student : toastMessages } = userFeedbackMessages
 
 const selectedStudents = ref<Student[]>([])
 const studentsToShow = computed(() => studentStore.searchedStudents.length ? studentStore.searchedStudents : studentStore.students)
@@ -102,10 +106,10 @@ const EditStudent = async (studentObj: Student) => {
         await backend.updateStudent(studentObj)
         await studentStore.populateStudents(studentToEdit.value?.class_id!)
         showStudentDialog.value = false
-        toast.add({ severity: 'success', summary: 'تم تعديل القسم بنجاح', life: 3000 })
+        toast.add({ severity: 'success', summary: toastMessages.updateSuccess , life: 3000 })
 
     } catch (error) {
-        toast.add({ severity: 'error', summary: 'حدث خطأ أثناء تعديل معلومات القسم', life: 3000 })
+        toast.add({ severity: 'error', summary: toastMessages.updateFailed, life: 3000 })
 
     }
 }
@@ -114,146 +118,35 @@ const createNewStudent = async (newStudent: NewStudent) => {
         await backend.createStudent(newStudent)
         await studentStore.populateStudents(newStudent.class_id)
         showStudentDialog.value = false
-        toast.add({ severity: 'success', summary: 'تمت إضافة التلميذ بنجاح', life: 3000 })
+        toast.add({ severity: 'success', summary: toastMessages.addSuccess, life: 3000 })
 
     } catch (error) {
-        toast.add({ severity: 'error', summary: 'حدث خطأ أثناء إنشاء القسم', life: 3000 })
+        toast.add({ severity: 'error', summary: toastMessages.addFailed, life: 3000 })
 
     }
 }
 const deleteStudents = async (students: Student[]) => {
-    try {
-        const studentIds = students.map((student) => student.id)
-        await backend.deleteStudents(studentIds);
-        studentStore.populateClasses()
-        selectedStudents.value = []
-        toast.add({ severity: "success", summary: 'مجحت المهمة', detail: 'لم يتم حذف ما تم تحديده ', life: 3000 })
-    }
-    catch (err) {
-        toast.add({ severity: "error", summary: 'فشلت المهمة', detail: 'تم حذف ما تم تحديده بنجاح', life: 3000 })
-    }
-
+    const studentIds = students.map((student) => student.id)
+    await backend.deleteStudents(studentIds);
 }
 
-onMounted(() => {
-    ProductService.getProducts().then((data) => (products.value = data));
+const transferStudents = async (students: Student[], classId : number) =>{
+const studentIds = students.map((student) => student.id)
+    await backend.deleteStudents(studentIds);
+}
+const useDeleteConfirm = useConfirmHandler(deleteStudents, studentStore.populateClasses)
+const useTransferConfirm = useConfirmHandler(transferStudents, studentStore.populateClasses)
 
-});
 
-const products = ref();
-const productDialog = ref(false);
-const deleteProductDialog = ref(false);
-const deleteProductsDialog = ref(false);
-const product = ref({});
+
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
 });
-const submitted = ref(false);
-const statuses = ref([
-    { label: 'INSTOCK', value: 'instock' },
-    { label: 'LOWSTOCK', value: 'lowstock' },
-    { label: 'OUTOFSTOCK', value: 'outofstock' }
-]);
 
-function openNew() {
-    product.value = {};
-    submitted.value = false;
-    productDialog.value = true;
-}
 
-function hideDialog() {
-    productDialog.value = false;
-    submitted.value = false;
-}
-
-function saveProduct() {
-    submitted.value = true;
-
-    if (product?.value.name?.trim()) {
-        if (product.value.id) {
-            product.value.inventoryStatus = product.value.inventoryStatus.value ? product.value.inventoryStatus.value : product.value.inventoryStatus;
-            products.value[findIndexById(product.value.id)] = product.value;
-            toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-        } else {
-            product.value.id = createId();
-            product.value.code = createId();
-            product.value.image = 'product-placeholder.svg';
-            product.value.inventoryStatus = product.value.inventoryStatus ? product.value.inventoryStatus.value : 'INSTOCK';
-            products.value.push(product.value);
-            toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
-        }
-
-        productDialog.value = false;
-        product.value = {};
-    }
-}
-
-function editProduct(prod) {
-    product.value = { ...prod };
-    productDialog.value = true;
-}
-
-function confirmDeleteProduct(prod) {
-    product.value = prod;
-    deleteProductDialog.value = true;
-}
-
-function deleteProduct() {
-    products.value = products.value.filter((val) => val.id !== product.value.id);
-    deleteProductDialog.value = false;
-    product.value = {};
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-}
-
-function findIndexById(id) {
-    let index = -1;
-    for (let i = 0; i < products.value.length; i++) {
-        if (products.value[i].id === id) {
-            index = i;
-            break;
-        }
-    }
-
-    return index;
-}
-
-function createId() {
-    let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < 5; i++) {
-        id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return id;
-}
 
 function exportCSV() {
     dt.value.exportCSV();
 }
 
-function confirmDeleteSelected() {
-    deleteProductsDialog.value = true;
-}
-
-function deleteSelectedProducts() {
-    products.value = products.value.filter((val) => !selectedStudents.value.includes(val));
-    deleteProductsDialog.value = false;
-    selectedStudents.value = null;
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-}
-
-function getStatusLabel(status) {
-    switch (status) {
-        case 'INSTOCK':
-            return 'success';
-
-        case 'LOWSTOCK':
-            return 'warn';
-
-        case 'OUTOFSTOCK':
-            return 'danger';
-
-        default:
-            return null;
-    }
-}
 </script>
