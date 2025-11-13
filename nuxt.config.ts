116cc81a58bdbf5eab3a1f0539986389c8ef711a
Abdou <2000abdallah2014@gmail.com>
2025-11-13 18:53:09 +0100

    added vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import Theme from '@primeuix/themes/aura';
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: false},
-    modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss',"@pinia/nuxt"],
+    modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss','@pinia/nuxt','@vueuse/nuxt'],
     srcDir: 'src/',
     css: [
         '/assets/styles.scss',
