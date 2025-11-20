55c1cf8512626eca04bfe39a4fe6cb5f7d8ab721
Abdou <2000abdallah2014@gmail.com>
2025-11-20 21:13:23 +0100

    update get absences API route

--- src/server/api/absences/index.get.ts
+++ src/server/api/absences/index.get.ts
@@ -1,9 +1,18 @@
 import db from "~/db/db";
-import type { LocalAbsence } from "~/data/types";
+import type { EventQueryFilters, LocalAbsence } from "~/data/types";
+import useDBUtils from "~/composables/useDBUtils";
 
-export default defineEventHandler(async(event) => {
-  //const body = await readBody(event) // time filter / class filter / offset 
-  const stmt = db.prepare(`
+export default defineEventHandler(async (event) => {
+  const { buildWhereQuery } = useDBUtils();
+
+  const query = getQuery<EventQueryFilters>(event); // time filter / class filter / offset
+  const { limit = 20, offset = 0 } = query;
+  const { where, params } = buildWhereQuery(query, "absence");
+  console.log(query, where, params);
+
+  const stmt = db
+    .prepare(
+      `
     SELECT
     s.first_name,
     s.last_name,
@@ -12,11 +21,16 @@ export default defineEventHandler(async(event) => {
     c.school_level,
     c.section,
     a.*
-FROM
-    absence a
+    FROM absence a
     INNER JOIN student s ON s.id = a.student_id
     INNER JOIN class c ON c.id = s.class_id
-`);
+    ${where ? where : ""}
+    ORDER BY a.date DESC
+    LIMIT ? OFFSET ?
+`
+    )
+    .bind(...params, limit, offset);
+    console.log(stmt);
   const absences = stmt.all();
   return absences as LocalAbsence[];
 });
