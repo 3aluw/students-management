2d160df4e0578422095b64858a361f6a486a99a8
Abdou <2000abdallah2014@gmail.com>
2026-04-01 19:49:27 +0100

    refactor : call student repo from studentService

--- src/server/services/studentService.ts
+++ src/server/services/studentService.ts
@@ -1,1 +1,9 @@
-export const studentService = {}
+import { ClassPromotionMap, Student } from "~/data/types";
+import { studentRepo } from "../repositories/studentRepo";
+
+export const studentService = {
+  promoteStudents(promotionMap: ClassPromotionMap, repeaters: Student[]) {
+    const repeatersIds = repeaters.map((student) => student.id);
+    studentRepo.handlesStudentsPromotion(promotionMap, repeatersIds);
+  },
+};
