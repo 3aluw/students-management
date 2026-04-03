f3c085f66992bb6c1b535b3bfd54b27ebc40a0de
Abdou <2000abdallah2014@gmail.com>
2026-04-03 11:24:34 +0100

    format return object in case of error at API layer

--- src/server/api/season/index.post.ts
+++ src/server/api/season/index.post.ts
@@ -1,20 +1,24 @@
-import type {
-  EditSchoolSeason,
-  NewSchoolSeason,
-  NewSeasonPayload,
-} from "~/data/types";
-import db from "~/db/db";
+import type { EditSchoolSeason, NewSeasonPayload } from "~/data/types";
 import useDBUtils from "../../../composables/useDBUtils";
 import { seasonRepo } from "~/server/repositories/seasonRepo";
 import { seasonService } from "~/server/services/seasonService";
+
 export default defineEventHandler(async (event) => {
-  const { generateDBSetClause } = useDBUtils();
 
   const reqBody = await readBody<EditSchoolSeason | NewSeasonPayload>(event);
 
   // if no id : Create a new item
   if (!("id" in reqBody)) {
-    seasonService.runNewSeasonWorkflow(reqBody);
+    try {
+      return seasonService.runNewSeasonWorkflow(reqBody);
+    } catch (error: any) {
+      return {
+        success: false,
+        step: error?.step || "غير محدد",
+        message:
+          error?.message || "حدث خطأ غير متوقع أثناء إنشاء الموسم الجديد",
+      };
+    }
   } // if id : item exists So update it
   else {
     seasonRepo.editSeason(reqBody);
