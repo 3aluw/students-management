995b70f6185e195c6d4e01d6bc43ccb26b172eed
3aluw <2000abdallah2014@gmail.com>
2026-05-20 11:19:34 +0100

    refactor: replace manual absence retrieval with absenceService for improved efficiency

--- src/server/api/absences/index.get.ts
+++ src/server/api/absences/index.get.ts
@@ -1,49 +1,19 @@
-import db from "~/db/db";
-import type { EventQueryFilters, LocalAbsence } from "~/data/types";
+import type { EventQueryFilters } from "~/data/types";
+import { absenceService } from "~/server/services/absenceService";
 import useDBUtils from "~/composables/useDBUtils";
-
-type TotalRow = { total: number };
+const { logError, toSafeError } = useDBUtils();
 
 export default defineEventHandler(async (event) => {
-  const { buildWhereQuery } = useDBUtils();
 
   const query = getQuery<EventQueryFilters>(event); // time filter / class filter / offset
-  const { limit = 20, offset = 0 } = query;
-  const { where, params } = buildWhereQuery(query, "absence");
-  
-  const stmt = db
-    .prepare(
-      `
-    SELECT
-    s.first_name,
-    s.last_name,
-    s.class_id,
-    c.grade,
-    c.school_level,
-    c.section,
-    a.*
-    FROM absence a
-    INNER JOIN student s ON s.id = a.student_id
-    INNER JOIN class c ON c.id = s.class_id
-    ${where ? where : ""}
-    ORDER BY a.date DESC
-    LIMIT ? OFFSET ?
-`
-    )
-    .bind(...params, Number(limit), Number(offset));
-  const absences = stmt.all()  as LocalAbsence[];
-
-  const stmtTotal = db
-    .prepare(
-    `SELECT COALESCE(COUNT(*), 0) AS total
-    FROM absence a
-    INNER JOIN student s ON s.id = a.student_id
-    INNER JOIN class c ON c.id = s.class_id
-    ${where ? where : ""}
-    ORDER BY a.date DESC`
-    )
-    .bind(...params);
-  const total = (stmtTotal.get() as TotalRow).total;
-
-  return {total, absences};
+  try {
+    return absenceService.getAbsences(query);
+  }
+  catch (err) {
+    logError("Error fetching absences:", err, event.path, query);
+    const safeError = createError(toSafeError(err, "فشلت عملية إيجاد الغيابات"));
+    return sendError(
+      event, safeError
+    );
+  }
 });
