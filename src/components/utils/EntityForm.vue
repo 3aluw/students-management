b467552c9beb405b28818a88907824381cbd965d
3aluw <2000abdallah2014@gmail.com>
2026-05-22 12:30:12 +0100

    refactor : Usage of imported schema instead of hard coded ones

--- src/components/utils/EntityForm.vue
+++ src/components/utils/EntityForm.vue
@@ -121,7 +121,6 @@ import { useToast } from 'primevue/usetoast';
 import type { NewClass, NewStudent, Student, Class  } from '~/data/types';
 import { schoolLevelOptions } from '~/data/static';
 import type { FormSubmitEvent } from "@primevue/forms"
-const { getRequiredFieldMessage } = useDataUtils()
 const {newStudentSchema, newClassSchema} = useZodSchema()
 const studentStore = useStudentStore();
 
@@ -152,24 +151,8 @@ const emit= defineEmits<{
 const resolver = computed(() => props.entityType == 'student' ? zodResolver(studentZodSchema) :
     zodResolver(classZodSchema)
 );
-const studentZodSchema = z.object({
-    status: z.enum(['active', 'graduated', 'dropped', 'transferred'], { error: getRequiredFieldMessage("status") }),
-    first_name: z.string({ error: getRequiredFieldMessage("first_name") }).min(3, { message: 'يجب إدخال اسم الطالب كاملا' }),
-    last_name: z.string({ error: getRequiredFieldMessage("last_name") }).min(3, { message: 'يجب إدخال اللقب كاملا ' }),
-    father_name: z.string({ error: getRequiredFieldMessage("father_name") }).min(3, { message: 'يجب استكمال اسم الأب ' }),
-    grandfather_name: z.string({ error: getRequiredFieldMessage("grandfather_name") }).min(3, { message: 'يجب استكمال اسم الجد ' }),
-    class_id: z.number({ error: getRequiredFieldMessage("class_id") }),
-    sex: z.literal(['F', 'M'], { error: getRequiredFieldMessage("sex") }),
-    phone_number: z.string({ error: getRequiredFieldMessage("phone_number") }).length(10, { message: 'يجب إدخال رقم هاتف صحيح ' }),
-    birth_date: z.date({ error: getRequiredFieldMessage("birth_date") }).transform(d => d.getTime()),
-    address: z.string({ error: getRequiredFieldMessage("address") }).min(10, { message: 'يجب إدخال العنوان بدقة ' }),
-}) satisfies z.ZodType<NewStudent>
-
-const classZodSchema = z.object({
-    grade: z.number({ error: getRequiredFieldMessage("grade") }).max(10, { message: 'يرجى إدخال مستوى منطقي' }),
-    school_level : z.literal(['primary','middle','high'], {error: getRequiredFieldMessage("school_level")}),
-    section: z.string({ error: getRequiredFieldMessage("section") })
-}) satisfies z.ZodType<NewClass>
+const studentZodSchema = newStudentSchema satisfies z.ZodType<NewStudent>
+const classZodSchema = newClassSchema satisfies z.ZodType<NewClass>
 
 
 
