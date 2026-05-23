7113cb39f790d5e013707de4431d47853b086f56
3aluw <2000abdallah2014@gmail.com>
2026-05-23 12:21:36 +0100

    refactor: Added validation for student operations

--- src/server/api/students/index.post.ts
+++ src/server/api/students/index.post.ts
@@ -2,21 +2,36 @@ import { EditStudent, BatchEditStudent, NewStudent } from "~/data/types";
 import useDBUtils from "~/composables/useDBUtils";
 import { studentService } from "~/server/services/studentService";
 import type { H3Error } from "h3";
+import { formatError, ZodError } from "zod";
+import useZodSchema from "~/composables/useZodSchema";
+import { z } from "zod";
+type Operation = "create" | "update" | "batch update";
 
 export default defineEventHandler(async (event) => {
-  const {  toSafeError, logError } = useDBUtils();
-
+  const { toSafeError, logError, formatZodError } = useDBUtils();
+  const { studentSchemas } = useZodSchema()
   const reqBody = await readBody<NewStudent | EditStudent | BatchEditStudent>(
     event
   );
+
+  let operation: Operation = "ids" in reqBody ? "batch update" :
+    !("id" in reqBody) ? "create" : "update";
+
+
+  const schemaMap: Record<Operation, z.ZodTypeAny> = {
+    create: studentSchemas.newStudentSchema,
+    update: studentSchemas.editStudentSchema,
+    'batch update': studentSchemas.batchEditStudentSchema,
+  };
+
   try {
+    const parsed = schemaMap[operation].parse(reqBody);
     if ("ids" in reqBody) {
       return studentService.updateStudents(reqBody);
     }
     // if no id : Create a new item
     if (!("id" in reqBody)) {
       return studentService.createStudent(reqBody);
-
     }
     // if id : item exists So update it
     else {
@@ -24,10 +39,22 @@ export default defineEventHandler(async (event) => {
     }
   }
   catch (error) {
+
+    // 1. validation error branch
+    if (error instanceof ZodError) {
+      const formattedError = formatZodError(error)[0].message + (formatError.length > 1 ? `كما يرجى التأكد من الحقول التالية ` : '')
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: formattedError,
+
+        })
+      );
+    }
     logError("Error updating student:", error, event.path, reqBody);
 
-    const reqMode = "ids" in reqBody ? "batch update" : !("id" in reqBody) ? "create" : "update";
-    const errorMessageTitle = reqMode === "create" ? " إنشاء الطالب" : reqMode === "update" ? " تحديث معلومات الطالب" : "تعديل الطلاب المحددين";
+    const errorMessageTitle = operation === "create" ? " إنشاء الطالب" : operation === "update" ? " تحديث معلومات الطالب" : "تعديل الطلاب المحددين";
     const errorMessage = (error as H3Error)?.statusMessage ?? "حدث خطأ أثناء " + errorMessageTitle
     const safeError = createError(toSafeError(error, "حدث خطأ أثناء " + errorMessage));
     return sendError(event, safeError);
