3573a320279814963b759c617ac1cd99c25e402a
3aluw <2000abdallah2014@gmail.com>
2026-05-30 18:58:22 +0100

    refactor: Added validation to class POST endpoint

--- src/server/api/classes/index.post.ts
+++ src/server/api/classes/index.post.ts
@@ -1,14 +1,26 @@
 import type { EditClass, NewClass } from "~/data/types";
-import useDBUtils from "../../../composables/useDBUtils";
+import useDBUtils from "~/composables/useDBUtils";
 import { classService } from "~/server/services/classService";
 import type { H3Error } from "h3";
+import useZodSchema from "~/composables/useZodSchema";
+import { z } from "zod";
+type Operation = "create" | "update";
 
-const { logError, toSafeError } = useDBUtils();
 export default defineEventHandler(async (event) => {
+const { logError, toSafeError } = useDBUtils();
+  const { classSchemas } = useZodSchema()
 
   const reqBody = await readBody<NewClass | EditClass>(event);
 
+  let operation: Operation = !("id" in reqBody) ? "create" : "update";
+
+  const schemaMap: Record<Operation, z.ZodTypeAny> = {
+    create: classSchemas.newClassSchema,
+    update: classSchemas.editClassSchema,
+  };
+
   try {
+    schemaMap[operation].parse(reqBody);
     if ("id" in reqBody) {
       return classService.editClass(reqBody);
     }
@@ -21,8 +33,8 @@ export default defineEventHandler(async (event) => {
 
     const reqMode = "id" in reqBody ? 'edit' : 'create'
     const errorMessageTitle = reqMode === 'edit' ? "تعديل القسم" : " إضافة القسم"
-        const errorMessage = (error as H3Error)?.message ?? "حدث خطأ أثناء " + errorMessageTitle
+    const errorMessage = (error as H3Error)?.message ?? "حدث خطأ أثناء " + errorMessageTitle
     throw createError(toSafeError(error, errorMessage));
-    
+
   }
 });
