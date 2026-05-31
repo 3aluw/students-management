4a5374943245219a4935275c710294fed4890167
3aluw <2000abdallah2014@gmail.com>
2026-05-31 21:42:55 +0100

    refactor: Added validation to season POST endpoint

--- src/server/api/season/index.post.ts
+++ src/server/api/season/index.post.ts
@@ -1,15 +1,27 @@
 
-import type { EditSchoolSeason, NewSeasonPayload } from "~/data/types";
+import type { BackendValidationError, EditSchoolSeason, NewSeasonPayload } from "~/data/types";
 import { seasonService } from "~/server/services/seasonService";
 import useDBUtils from "~/composables/useDBUtils";
 import type { H3Error } from "h3";
+import { ZodError } from "zod";
+import useZodSchema from "~/composables/useZodSchema";
+import { z } from "zod";
+type Operation = "create" | "update";
 
 export default defineEventHandler(async (event) => {
-const { logError, toSafeError } = useDBUtils();
+  const { logError, toSafeError } = useDBUtils();
+  const { seasonSchemas } = useZodSchema()
 
   const reqBody = await readBody<EditSchoolSeason | NewSeasonPayload>(event);
 
+  let operation: Operation = !("id" in reqBody) ? "create" : "update";
+  const schemaMap: Record<Operation, z.ZodTypeAny> = {
+    create: seasonSchemas.NewSeasonPayloadSchema,
+    update: seasonSchemas.editSeasonSchema,
+  };
+
   try {
+    schemaMap[operation].parse(reqBody);
     // if no id : Create a new item
     if (!("id" in reqBody)) {
       return seasonService.runNewSeasonWorkflow(reqBody);
@@ -20,9 +32,23 @@ const { logError, toSafeError } = useDBUtils();
     }
   }
   catch (error) {
+    // 1. validation error branch
+    if (error instanceof ZodError) {
+      throw createError({
+        statusCode: 400,
+        message: 'مشكلة في البيانات المرسلة',
+        statusMessage: "failed validation",
+        data: {
+          issues: error.issues
+        }
+      } as BackendValidationError)
+
+    }
+    // 2. Business / Database error branch 
+
     logError("Error updating student:", error, event.path, reqBody);
 
-    const errorMessageTitle = "id" in reqBody  ? " تحديث الموسم " : 'إنشاء الموسم الجديد'
+    const errorMessageTitle = "id" in reqBody ? " تحديث الموسم " : 'إنشاء الموسم الجديد'
     const errorMessage = (error as H3Error)?.message ?? "حدث خطأ أثناء " + errorMessageTitle
 
     throw createError(toSafeError(error, "حدث خطأ أثناء " + errorMessage));
