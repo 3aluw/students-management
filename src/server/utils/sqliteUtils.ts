2b7d1d4fb1c8e2e3216e83dec6598c316b56f49f
3aluw <2000abdallah2014@gmail.com>
2026-06-04 22:32:16 +0100

    refactor: Moved sqlite utils from useDBUtils to server/utils

---
+++ src/server/utils/sqliteUtils.ts
@@ -0,0 +1,66 @@
+import type { EventQueryFilters } from "~/data/types";
+
+export const generateDBSetClause = <T extends Object>(object: T) => {
+    // fields is an object like { name: 'Alice', age: 30, status: 'active' }
+    const keys = Object.keys(object) as (keyof object)[];
+    // Create "col1 = ?, col2 = ?, ..." for the SQL
+    return keys.map((key) => `${key} = ?`).join(", ");
+};
+
+export const generateDBInClause = (num: number) => Array(num).fill("?").join(",");
+
+export const buildWhereQuery = <T extends EventQueryFilters>(
+    queryObj: T,
+    eventType: "absence" | "lateness",
+) => {
+    const clauses: string[] = [];
+    const params: any[] = [];
+    const defaultTableAbbr = eventType === "absence" ? "a." : "l.";
+    for (const key of Object.keys(queryObj) as (keyof T)[]) {
+        const value = queryObj[key];
+        if (value === undefined) continue;
+        if (key === "limit" || key === "offset") continue;
+
+        if (key === "minDate") {
+            clauses.push(`${defaultTableAbbr}date >= ?`);
+            params.push(Number(value));
+            continue;
+        }
+        if (key === "maxDate") {
+            clauses.push(`${defaultTableAbbr}date <= ?`);
+            params.push(Number(value));
+            continue;
+        }
+        if (key === "classId") {
+            clauses.push(`s.class_id = ?`);
+            params.push(Number(value));
+            continue;
+        }
+        if (key === "name") {
+            clauses.push("first_name LIKE ? OR last_name LIKE ?");
+            params.push(`%${value}%`, `%${value}%`);
+            continue;
+        }
+        clauses.push(`${String(key)} = ?`);
+        params.push(isNaN(Number(value)) ? value : Number(value));
+    }
+
+    const where = clauses.length ? `WHERE ${clauses.join(" AND ")}` : "";
+    return { where, params };
+};
+
+// ========== INTERNAL: takes an array and returns a SQL values list : (1),(2),... ==========
+const toSqlValuesList = (values: ([string, number] | number)[]) =>
+    values.map((value) => `(${value})`).join(",");
+
+// ========== generates SQL values for a CTE while taking into account empty CTEs ==========
+export const generateSqlCTEValues = (
+    values: ([string, number] | number)[],
+    columnsCount: number,
+) => {
+    if (values.length) return `${toSqlValuesList(values)}`;
+    else {
+        const nulls = Array(columnsCount).fill("NULL").join(", ");
+        return `SELECT ${nulls} WHERE 0`;
+    }
+};
