14e426b2765a203e1c22d77f0ef59e6e4410d514
3aluw <2000abdallah2014@gmail.com>
2026-06-05 09:29:40 +0100

    refactor : Moved arabic properties funcitons from Data composable to utils folder

---
+++ src/utils/arabic-properties.ts
@@ -0,0 +1,26 @@
+import { arabicProperties } from "~/data/static";
+  
+  // ========== Fields / its Arabic translations functions ==========
+  /*Internal */
+  const formatRequiredFieldMessage = (
+    arabicFieldName: string,
+    type: "text" | "choice" = "text",
+  ) =>
+    type === "text"
+      ? `يجب إدخال ${arabicFieldName}`
+      : type == "choice"
+        ? `يجب اختيار ${arabicFieldName}`
+        : "هذا الحقل مطلوب";
+
+  export const getRequiredFieldMessage = (
+    fieldName: string,
+    type: "text" | "choice" = "text",
+  ) => {
+    const arabicPropertyName = getPropertyArabicName(fieldName)
+    return arabicPropertyName
+      ? formatRequiredFieldMessage(arabicPropertyName, type)
+      : `يجب إدخال الحقل التالي: ${fieldName} `;
+  };
+  export const getPropertyArabicName = (fieldName: string) => fieldName in arabicProperties ? arabicProperties[fieldName as keyof typeof arabicProperties] : undefined
+
+
