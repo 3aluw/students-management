1e9e4f2f680c16645c5a97e2f8baefb221c2e294
3aluw <2000abdallah2014@gmail.com>
2026-06-12 09:50:48 +0100

    feat: Added inactive student deletion / transfer features

--- src/pages/archive.vue
+++ src/pages/archive.vue
@@ -6,12 +6,20 @@
             <template #toolbar>
                 <Toolbar class="mb-6">
                     <template #start>
-                        <IconField>
-                            <InputIcon>
-                                <i class="pi pi-search" />
-                            </InputIcon>
-                            <InputText v-model.trim="globalSearchValue" placeholder="بحث عام" type="search" />
-                        </IconField>
+                        <Button :disabled="selectedStudents.length == 0" label="حذف" icon="pi pi-trash" iconPos="right"
+                            severity="secondary" class="mx-2" @click="" />
+                        <Button :disabled="selectedStudents.length == 0" label="نقل إلى" icon="pi pi-undo"
+                            @click="toggleTransferStudentsMenu" aria-haspopup="true" aria-controls="overlay_menu"
+                            iconPos="right" severity="secondary" class="mx-2" />
+                        <Menu ref="transferStudentsMenu" id="overlay_menu" :model="studentStore.classOptions"
+                            :popup="true">
+                            <template #item="{ item }">
+                                <Button variant="text" severity="secondary"
+                                    @click="useTransferConfirm.requestAction(selectedStudents, [item.value])"> {{
+                                        item.label
+                                    }}</Button>
+                            </template>
+                        </Menu>
                     </template>
                 </Toolbar>
             </template>
@@ -21,7 +29,7 @@
                         <h4 class="m-0">{{ 'الطلبة المغادرون' }}</h4>
 
                         <SelectButton name="status" :options="statusFilterOptions" optionLabel="label"
-                            optionValue="value" v-model="modalFilters.status" />
+                            optionValue="value" v-model="modalFilters.status" :allow-empty="false" />
 
                         <DatePicker :showButtonBar="true" :manualInput="false" placeholder="سنة المغادرة"
                             v-model="modalFilters.exited_at_Year" view="year" dateFormat="yy">
@@ -40,49 +48,168 @@
         </UtilsStudentsTableNew>
     </div>
 
+    <UtilsConfirmDialog header="استرجاع الطلبة"
+        message="هل أنت متأكد من رغبتك في  استرجاع الطلبة المحددين ونقلهم إلى القسم ؟" :danger="true"
+        v-model="useTransferConfirm.showConfirm.value" @confirm="useTransferConfirm.confirmAction" />
+    <UtilsConfirmDialog header="استرجاع الطلبة" message="هل أنت متأكد من رغبتك حذف الطلبة المحددين نهائيا  ؟"
+        :danger="true" v-model="useDeleteConfirm.showConfirm.value" @confirm="useDeleteConfirm.confirmAction" />
 </template>
 
 <script setup lang="ts">
-import { statusFilterOptions } from "~/data/static"
-import type { InactiveStudentQueryFilters, Student } from '~/data/types';
-import { watchDebounced } from "@vueuse/core";
+import { watchDebounced } from '@vueuse/core';
+import { useToast } from 'primevue/usetoast';
+
+import {
+    statusFilterOptions,
+    userFeedbackMessages
+} from '~/data/static';
+
+import type {
+    BatchEditStudent,
+    InactiveStudentQueryFilters,
+    Student
+} from '~/data/types';
+
+import { useStudentStore } from '~/store/studentStore';
+
+/* -------------------------------------------------------------------------- */
+/*                                Stores                                      */
+/* -------------------------------------------------------------------------- */
+
+const studentStore = useStudentStore();
+
+const backend = useBackend();
+const toast = useToast();
+
+/* -------------------------------------------------------------------------- */
+/*                                Messages                                    */
+/* -------------------------------------------------------------------------- */
+
+const { student: toastMessages } = userFeedbackMessages;
+
+/* -------------------------------------------------------------------------- */
+/*                                Types                                       */
+/* -------------------------------------------------------------------------- */
 
 type ClientFilters = {
     name: string;
-    status: "graduated" | "dropped" | "transferred";
+    status: 'graduated' | 'dropped' | 'transferred';
     exited_at_Year: Date | undefined;
-}
-const Backend = useBackend()
+};
 
+/* -------------------------------------------------------------------------- */
+/*                                State                                       */
+/* -------------------------------------------------------------------------- */
 
+const studentsToShow = ref<Student[]>([]);
+const selectedStudents = ref<Student[]>([]); //passed from the table component
 
-const studentsToShow = ref<Student[]>([])
-const getStudents = async (filters: InactiveStudentQueryFilters) => {
-    studentsToShow.value = await Backend.getStudents(filters)
-}
-onMounted(async () => {
-    const initialFilters: InactiveStudentQueryFilters = {
-        status: "graduated"
-    }
-    getStudents(initialFilters)
-})
-const selectedStudents = ref()
-const globalSearchValue = ref('')
-const tableSearchValue = ref('')
+const globalSearchValue = ref('');
+const tableSearchValue = ref('');
 
 const modalFilters = ref<ClientFilters>({
-    status: "graduated",
+    status: 'graduated',
     exited_at_Year: undefined,
     name: ''
-})
+});
 
-watchDebounced(modalFilters.value, () => {
-    console.log(modalFilters.value)
-    const dbFilters: InactiveStudentQueryFilters = {
+/* -------------------------------------------------------------------------- */
+/*                              Data Fetching                                 */
+/* -------------------------------------------------------------------------- */
+
+const getStudents = async (
+    filters: InactiveStudentQueryFilters
+) => {
+    studentsToShow.value =
+        await backend.getStudents(filters);
+};
+
+const formatModalFilters =
+    (): InactiveStudentQueryFilters => ({
         ...modalFilters.value,
-        exited_at_Year: modalFilters.value.exited_at_Year?.getTime()
-    }
-    getStudents(dbFilters)
-}, { debounce: 500 })
+        exited_at_Year:
+            modalFilters.value.exited_at_Year?.getTime()
+    });
+
+onMounted(() => {
+    getStudents({
+        status: 'graduated'
+    });
+});
+
+watchDebounced(
+    modalFilters.value,
+    () => {
+        getStudents(formatModalFilters());
+    },
+    { debounce: 500 }
+);
+
+/* -------------------------------------------------------------------------- */
+/*                              Shared Actions                                */
+/* -------------------------------------------------------------------------- */
+/* runs after Transfer or Delete actions */
+const afterAction = () => {
+    getStudents(formatModalFilters());
+    selectedStudents.value = [];
+};
+
+/* -------------------------------------------------------------------------- */
+/*                            Transfer Students                               */
+/* -------------------------------------------------------------------------- */
+
+const transferStudentsMenu = ref();
+
+const toggleTransferStudentsMenu = (
+    event: Event
+) => {
+    transferStudentsMenu.value.toggle(event);
+};
+
+const transferStudents = async (
+    students: Student[],
+    classId: number
+) => {
+    const studentIds = students.map(
+        (student) => student.id
+    );
+
+    const reqBody: BatchEditStudent = {
+        class_id: classId,
+        status: 'active',
+        exited_at: null,
+        ids: studentIds
+    };
+
+    await backend.updateStudents(reqBody);
+};
+
+const useTransferConfirm = useConfirmHandler(
+    transferStudents,
+    afterAction,
+    toastMessages.transferSuccess,
+    toastMessages.transferFailed
+);
+
+/* -------------------------------------------------------------------------- */
+/*                              Delete Students                               */
+/* -------------------------------------------------------------------------- */
+
+const deleteStudents = async (
+    students: Student[]
+) => {
+    const studentIds = students.map(
+        (student) => student.id
+    );
+
+    await backend.deleteStudents(studentIds);
+};
+
+const useDeleteConfirm = useConfirmHandler(
+    deleteStudents,
+    afterAction,
+    toastMessages.deleteSuccess,
+    toastMessages.deleteFailed
+);
 
 </script>
