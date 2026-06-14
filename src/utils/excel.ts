7d9a577d7073599c4f3d20ba3e0e026300df43b7
3aluw <2000abdallah2014@gmail.com>
2026-06-14 12:42:32 +0100

    refactor: created transformEventToExcelVersion to created xlsx objects for event (lateness and absence)

--- src/utils/excel.ts
+++ src/utils/excel.ts
@@ -1,4 +1,4 @@
-import type { Student, XLSXStudent } from "~/data/types";
+import type { Absence, Lateness, LocalAbsence, LocalLateness, Student, XLSXAbsnece, XLSXLateness, XLSXStudent } from "~/data/types";
 
 export const transformStudentToExcelVersion = (student: Student): XLSXStudent => {
     const { class_id, exited_at, status, birth_date, ...rest } = student;
@@ -8,3 +8,22 @@ export const transformStudentToExcelVersion = (student: Student): XLSXStudent =>
         birth_date: new Date(birth_date),
     }
 };
+
+type ExcelEventVersion<T> = T extends LocalLateness ? XLSXLateness : XLSXAbsnece;
+export const transformEventToExcelVersion = <T extends LocalAbsence | LocalLateness>
+    (event: T): ExcelEventVersion<T> => {
+    const { reason, first_name, last_name } = event
+
+    const base: XLSXAbsnece = {
+        reason, first_name, last_name,
+        reason_accepted: Boolean(event.reason_accepted),
+        date: new Date(event.date),
+        class: "",
+    }
+
+    if ("late_by" in event) {
+        return { ...base, late_by: event.late_by } as ExcelEventVersion<T>
+    }
+
+    return base as ExcelEventVersion<T>
+}
