812defa8c64b059527274dd101f0c7fc49a86f00
3aluw <2000abdallah2014@gmail.com>
2026-06-18 10:58:22 +0100

    fix: force ids to array at students GET query

--- src/server/api/students/index.get.ts
+++ src/server/api/students/index.get.ts
@@ -1,13 +1,14 @@
 import { studentService } from "~/server/services/studentService";
-import type { StudentsQueryFilters } from "~/data/types"
+import type { StudentsQueryFilters } from "~/data/types";
 export default defineEventHandler((event) => {
-
-
-  const query = getQuery<StudentsQueryFilters | { ids: number[] }>(
-    event,
-  );
+  const query = getQuery<StudentsQueryFilters | { ids: number[] }>(event);
   try {
-    if ('ids' in query) query.ids = query.ids.map(Number)
+    if ("ids" in query)
+      query.ids = Array.isArray(query.ids)
+        ? query.ids.map(Number)
+        : query.ids
+          ? [query.ids].map(Number)
+          : [];
     return studentService.getStudents(query);
   } catch (err) {
     logError("Error fetching students:", err, event.path, query);
